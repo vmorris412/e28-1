404fc83af0919f016c9baa7beecd259ebb2a7183
Susan Buck <susan@susanbuck.net>
2020-10-05 12:23:33 -0400

    Word Scramble solution from the Week 5 assignment

---
+++ word-scramble/script.js
@@ -0,0 +1,49 @@
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        words: [
+            ['apple', 'Sometimes red, sometimes delicious'],
+            ['washington', 'Rushmore’s left edge'],
+            ['pumpkin', 'Halloween’s favorite fruit'],
+            ['football', 'Play with your hands or feet, depending on your location']
+        ],
+        gameMode: 'start',
+        name: '',
+        hint: '',
+        word: '',
+        guess: '',
+        lastWord: '',
+        feedback: false,
+        correct: false,
+    },
+    computed: {
+        displayWord() {
+            var wordAsArray = this.word.split('');
+            wordAsArray.sort(() => Math.random() - 0.5);
+            return wordAsArray.join('');
+        }
+    },
+    methods: {
+        submitGuess() {
+            this.correct = this.guess == this.word;
+            this.feedback = true;
+        },
+        startGame() {
+            this.gameMode = 'play';
+            this.loadGame();
+        },
+        loadGame() {
+            this.feedback = false;
+            this.guess = '';
+
+            while (this.word === this.lastWord) {
+                this.choice = this.words[Math.floor(Math.random() * this.words.length)];
+                this.word = this.choice[0];
+                this.hint = this.choice[1];
+            }
+
+            this.lastWord = this.word;
+        }
+    }
+});
